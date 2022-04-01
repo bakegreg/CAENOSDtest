aee9195c85c9e6ebe2aeb1c2f99e7eaf848153c8
Greg Baker <bakegreg@umich.edu>
2022-03-31 22:11:13 -0400

    Screwed up the credential builder command.....

--- scripts/Set-NameAndProduct.ps1
+++ scripts/Set-NameAndProduct.ps1
@@ -9,7 +9,7 @@ $pw = $tsenv.Value("CaenAdPw")
 #the key is supposed to be integers separated by newlines but SCCM variables can't handle the newlines. Used commas instead so the data needs to converted. Writes it to disk so it can be read in in as a byte string
 $tsenv.Value("CaenAdPwKey") -replace ",","`n" | out-file .\key -encoding utf8 
 
-$credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $user, ($pw | get-content .\key)
+$credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $user, ($pw | convertto-securestring -key (get-content .\key))
 
 #Try connecting to AD
 $retries = 0
